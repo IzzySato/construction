2ac598924ef258ee68ac641c70bbcda5b827d12e
Izzy Sato <izzysato@Izzys-MacBook-Pro-3.local>
2022-03-11 13:54:28 -0800

    added navigation

--- components/Nav.js
+++ components/Nav.js
@@ -1,30 +1,30 @@
-import Link from 'next/link';
 import styles from '../styles/Nav.module.css';
 
 const Nav = () => {
-  return (
-    <div className={styles.wrapper}>
-      <div className={styles.phoneMenu}>
-        <div className={styles.phoneLine}></div>
-        <div className={styles.phoneLine}></div>
-        <div className={styles.phoneLine}></div>
-      </div>
-      <img className={styles.logo} alt="logo" src="../images/logo_w_2.svg"/>
-      <nav className={styles.nav}>
+
+  return(
+    // hamburger menu for mobile
+    <nav className={styles.mobileNav}>
+      <label htmlFor="checkMenu" className={styles.menuIcon}>
+        <div className={styles.menuLine}></div>
+        <div className={styles.menuLine}></div>
+        <div className={styles.menuLine}></div>
+      </label>
+      <input type="checkbox" id="checkMenu" className={styles.menu}/>
+      {/* menu list */}
         <ul className={styles.ul}>
           <li className={styles.li}>
-            <Link href='/'>Home</Link>
+            <a className={styles.a} id="home" href="/" >Home</a>
           </li>
           <li className={styles.li}>
-            <Link href='/about'>About us</Link>
+            <a className={styles.a} id="about" href="/about" >About us</a>
           </li>
           <li className={styles.li}>
-            <Link href='/contact'>Contact us</Link>
+              <a className={styles.a} id="contact" href="/contact" >Contact us</a>
           </li>
         </ul>
-      </nav>
-    </div>
-  )
+    </nav>
+  );
 };
 
 export default Nav;
